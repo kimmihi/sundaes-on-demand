8113a1c81c9dc41bab6fccf1f22440acd26e9c73
kimmeh <mhko0411@gmail.com>
2022-12-12 22:19:23 +0900

    popover 테스트 및 구현

--- src/pages/summary/components/SummaryForm/index.jsx
+++ src/pages/summary/components/SummaryForm/index.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 export default function SummaryForm() {
   const [checked, setChecked] = useState(false);
+  const [isOver, setIsOver] = useState(false);
+
   return (
     <form>
-      <div>
+      <div
+        style={{ position: "relative" }}
+        onMouseOver={() => setIsOver(true)}
+        onMouseLeave={() => setIsOver(false)}
+      >
         <label htmlFor="agree-checkbox">약관동의</label>
         <input
           type="checkbox"
@@ -11,6 +17,11 @@ export default function SummaryForm() {
           checked={checked}
           onChange={(e) => setChecked(e.target.checked)}
         />
+        {isOver && (
+          <div>
+            <p>주문을 완료해도 배달되지 않습니다.</p>
+          </div>
+        )}
       </div>
       <div>
         <button type="submit" disabled={!checked}>
--- src/pages/summary/components/SummaryForm/index.test.jsx
+++ src/pages/summary/components/SummaryForm/index.test.jsx
@@ -25,4 +25,18 @@ describe("Summary From Component Test", () => {
 
     expect(submitButton).toBeEnabled();
   });
+
+  test("popover test", async () => {
+    render(<SummaryForm />);
+    const user = userEvent.setup();
+    const checkbox = screen.getByRole("checkbox", { name: "약관동의" });
+    const nullPopover = screen.queryByText(
+      "주문을 완료해도 배달되지 않습니다."
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    await user.hover(checkbox);
+    const popover = screen.queryByText("주문을 완료해도 배달되지 않습니다.");
+    expect(popover).toBeInTheDocument();
+  });
 });
