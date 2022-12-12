82a287294a8c336e46b49c092bd76646b3af97dd
kimmeh <mhko0411@gmail.com>
2022-12-12 21:44:01 +0900

    fireEvent -> userEvent로 수정

--- src/pages/summary/components/SummaryForm/index.test.jsx
+++ src/pages/summary/components/SummaryForm/index.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "./index";
 
@@ -12,12 +13,14 @@ describe("Summary From Component Test", () => {
     expect(submitButton).toBeDisabled();
   });
 
-  test("enabled button after check", () => {
+  test("enabled button after check", async () => {
     render(<SummaryForm />);
     const checkbox = screen.getByRole("checkbox", { name: "약관동의" });
     const submitButton = screen.getByRole("button", { name: "주문" });
 
-    fireEvent.click(checkbox);
+    const user = userEvent.setup();
+
+    await user.click(checkbox);
     expect(checkbox).toBeChecked();
 
     expect(submitButton).toBeEnabled();
