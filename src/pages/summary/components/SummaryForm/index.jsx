27254a56511f225c4da9d704ffaa15c663e76358
kimmeh <mhko0411@gmail.com>
2022-12-12 21:18:45 +0900

    테스트 통과하도록 컴포넌트 작성

--- src/pages/summary/components/SummaryForm/index.jsx
+++ src/pages/summary/components/SummaryForm/index.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
 export default function SummaryForm() {
-  return <></>;
+  const [checked, setChecked] = useState(false);
+  return (
+    <form>
+      <div>
+        <label htmlFor="agree-checkbox">약관동의</label>
+        <input
+          type="checkbox"
+          id="agree-checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+      </div>
+      <div>
+        <button type="submit" disabled={!checked}>
+          주문
+        </button>
+      </div>
+    </form>
+  );
 }
